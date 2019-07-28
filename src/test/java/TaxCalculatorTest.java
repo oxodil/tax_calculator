1b423c4144610456b7a0be1b3fee3e33c739cfc4
MN <trading.acc71@gmail.com>
2019-07-29 01:29:39 +0200

    update on tests

--- src/test/java/TaxCalculatorTest.java
+++ src/test/java/TaxCalculatorTest.java
@@ -9,41 +9,51 @@ public class TaxCalculatorTest {
 
     @Test
     public void shouldThrowException() {
-
+        System.out.println("Test case 1: Testing an exception mechanism for incorrect price value");
         Boolean isExceptionHasBeenThrown = false;
         try {
             Product book = new Book("book", -12.49f, false);
         } catch (IllegalArgumentException e) {
             isExceptionHasBeenThrown = true;
-            System.out.println("Product's price can't be a negative number");
+            System.out.println("Product's price can't be a negative number.");
         }
 
         Assert.assertTrue("Exception has not been thrown.", isExceptionHasBeenThrown);
+        System.out.println("Setting a negative price value for a product does throw an IllegalArgumentException");
     }
 
     @Test
     public void shouldApply0TaxForNotImportedBook() {
+        System.out.println("Test case 2: Testing if not imported book will have 0 tax value.");
         Book book = new Book("book", 12.49f, false);
 
-        Assert.assertTrue("Ha not applied 0 tax but: " + book.getTotalTaxValue(), book.getTotalTaxValue() == 0);
+        Assert.assertTrue("Has not applied 0 tax but: " + book.getTotalTaxValue(), book.getTotalTaxValue() == 0);
+        System.out.println("Not imported book has 0 total tax value.");
     }
 
     @Test
     public void shouldApply5PercentTaxForImportedBook() {
         Book book = new Book("book", 15.49f, true);
+        System.out.println("Test case 3: Testing if not imported book will have 0 tax value.");
 
         Assert.assertTrue("Has not applied 5 tax but: " + book.getImportedGoodsTaxPercent(), book.getImportedGoodsTaxPercent()==5.00);
+        System.out.println("Imported Book has 5% tax.");
     }
 
     @Test
     public void shouldCalculateTotalValueForImportedCDPlayer() {
+        System.out.println("Test case 4: Testing if total price to pay for an imported CD player will be equal to 115.");
         SalesTaxedProduct cdPlayer = new SalesTaxedProduct("CD player", 100.00f, true);
 
         Assert.assertTrue("Total product value with tax is not equal to 115 but: " + cdPlayer.getPriceWithTax(), cdPlayer.getPriceWithTax() == 115.00);
+        System.out.println("Imported CD player total price is equal to: " + cdPlayer.getPriceWithTax() + " with included total tax of " + cdPlayer.getTotalTaxValue());
+
     }
 
     @Test
     public void shouldCalculateValuesTestCase1() {
+        System.out.println("Test case 5: Testing a given scenario Input1");
+
         Book book = new Book("book", 12.49f, false);
         SalesTaxedProduct musicCD = new SalesTaxedProduct("music CD", 14.99f, false);
         Food chocolateBar = new Food("chocolate bar", 0.85f, false);
@@ -64,10 +74,12 @@ public class TaxCalculatorTest {
                 "Total: 29.83";
 
         Assert.assertTrue("Receipt is not correct", expectedValue.equals(receipt));
+        System.out.println("Algorithm has calculated the total value and tax value correctly");
     }
 
     @Test
     public void shouldCalculateValuesTestCase2() {
+        System.out.println("Test case 6: Testing a given scenario Input2");
         Food importedBoxOfChocolates = new Food("imported box of chocolates", 10.00f, true);
         SalesTaxedProduct importedBottleOfPerfume = new SalesTaxedProduct("imported bottle of perfume", 47.50f, true);
 
@@ -85,10 +97,12 @@ public class TaxCalculatorTest {
                 "Total: 65.15";
 
         Assert.assertTrue("Receipt is not correct", expectedValue.equals(receipt));
+        System.out.println("Algorithm has calculated the total value and tax value correctly");
     }
 
     @Test
     public void shouldCalculateValuesTestCase3() {
+        System.out.println("Test case 7: Testing a given scenario Input3");
         SalesTaxedProduct importedBottleOfPerfume = new SalesTaxedProduct("imported bottle of perfume", 27.99f, true);
         SalesTaxedProduct bottleOfPerfume = new SalesTaxedProduct("bottle of perfume", 18.99f, false);
         MedicalProduct headachePills = new MedicalProduct("packet of headache pills", 9.75f, false);
@@ -112,5 +126,6 @@ public class TaxCalculatorTest {
                 "Total: 74.63";
 
         Assert.assertTrue("Receipt is not correct", expectedValue.equals(receipt));
+        System.out.println("Algorithm has calculated the total value and tax value correctly");
     }
 }
